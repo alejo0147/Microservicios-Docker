15907fec23062b48900fe2f7ffa0afeb27074551
Alejandro Parias <alejandroparmo@gmail.com>
2024-08-16 22:32:07 -0500

    WIP: Dockerización del microservico usuarios

--- msvc-usuarios/src/main/java/com/algorian/springcloud/msvc/usuarios/client/ICursoClientRest.java
+++ msvc-usuarios/src/main/java/com/algorian/springcloud/msvc/usuarios/client/ICursoClientRest.java
@@ -5,8 +5,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-//@FeignClient(name = "msvc-cursos", url="host.docker.internal:8002")
-@FeignClient(name = "msvc-cursos", url="localhost:8002")
+@FeignClient(name = "msvc-cursos", url="host.docker.internal:8002")
+//@FeignClient(name = "msvc-cursos", url="localhost:8002")
 public interface ICursoClientRest {
 
     @DeleteMapping("/eliminar-curso-usuario/{usuarioId}")
